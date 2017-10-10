d04ad512c764ec3c10724fe51e6ef900c99563e6
supermarioknight <hunter.dev.us@gmail.com>
2017-10-10 19:30:54 +1100

    chore(module): rename to chat codes

--- index.spec.js
+++ index.spec.js
@@ -3,25 +3,25 @@ const markup = require('./');
 
 describe('markup', () => {
   it('should generate markup', () => {
-    const gamecode = '[&AgFuLgEA]';
+    const chatcode = '[&AgFuLgEA]';
 
-    const actual = markup(gamecode);
+    const actual = markup(chatcode);
 
     assert.equal(actual, '<div data-armory-embeds="items" data-armory-ids="77422"></div>');
   });
 
   it('should use custom tag', () => {
-    const gamecode = '[&CvUbAAA=]';
+    const chatcode = '[&CvUbAAA=]';
 
-    const actual = markup(gamecode, { tag: 'span' });
+    const actual = markup(chatcode, { tag: 'span' });
 
     assert.equal(actual, '<span data-armory-embeds="skins" data-armory-ids="7157"></span>');
   });
 
   it('should return invalid text if invalid', () => {
-    const gamecode = '&AgGuLgEK]';
+    const chatcode = '&AgGuLgEK]';
 
-    const actual = markup(gamecode);
+    const actual = markup(chatcode);
 
     assert.equal(actual, '[invalid]');
   });
